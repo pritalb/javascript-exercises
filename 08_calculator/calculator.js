7096daec0f8f392e8065b8d9308a80ae1fce5eb1
pritalb <pritalbamnodkar@gmail.com>
2022-05-18 21:44:55 +0530

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let sum = 0;
+
+    for (let index = 0; index < array.length; index++) {
+        sum += array[index];
+    }
+
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    product = 1;
 
+    for (let index = 0; index < array.length; index++) {
+        product *= array[index];
+    }
+
+    return product;
 };
 
-const power = function() {
-	
+const power = function(base, pow) {
+	return base ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	fact = 1;
+
+    for (let i = 1; i < n + 1; i++) {
+        fact *= i;
+    }
+
+    return fact;
 };
 
 // Do not edit below this line
