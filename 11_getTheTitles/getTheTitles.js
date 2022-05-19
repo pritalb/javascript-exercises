f67aa74bc9815548b89ea1961d24e6fb77e02a6a
pritalb <pritalbamnodkar@gmail.com>
2022-05-19 18:52:04 +0530

    solved 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+	out = []
 
+	arr.map((book) => {
+		out.push(book['title'])
+	})
+
+	return out
 };
 
 // Do not edit below this line
