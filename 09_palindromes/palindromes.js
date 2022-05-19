e51e12281c727a443b5c213734119b5fa585cb0d
pritalb <pritalbamnodkar@gmail.com>
2022-05-19 18:29:37 +0530

    solved 09_palindromes.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const toAlphabetic = (str) => {
+	const alphabets = 'abcdefghijklmnopqrstuvwxyz'
+	const symbols = '!@#$%^&*()_+=-;:\'\",.<>/?\\|'
+	const strLower = str.toLowerCase()
+	let out = ''
 
+	for (let index = 0; index < strLower.length; index++) {
+		const currentLetter = strLower[index];
+
+		if ((alphabets.search(currentLetter) !== -1) && symbols.search(currentLetter) === -1 ) {
+			out += currentLetter;
+		}
+	}
+
+	return out
+}
+
+const reverseString = (str) => {
+	out = ''
+
+	for (let index = str.length - 1; index >= 0; index--) {
+		out += str[index]
+	}
+
+	return out
+}
+
+const palindromes = function (str) {
+	const onlyLettersStr = toAlphabetic(str)
+
+	return onlyLettersStr === reverseString(onlyLettersStr)
 };
 
 // Do not edit below this line
