302c49e0fd5fec0b6df5a21762811194ca7b4075
pritalb <pritalbamnodkar@gmail.com>
2022-05-19 18:46:38 +0530

    solved 10_fibonnacci.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+// returns a list of first n fibonacci numbers
+const fibonacciSequence = (n) => {
+	let fibs = [1, 1]
 
+	while (fibs.length < n) {
+		let currentFibs = fibs.length
+		fibs.push(fibs[currentFibs - 1] + fibs[currentFibs - 2])
+	}
+
+	return fibs
+}
+
+const fibonacci = function(num) {
+	const n = Number(num)
+
+	if (n < 0) {
+		return 'OOPS'
+	}
+
+	const fibs = fibonacciSequence(n)
+
+	return fibs[n - 1]
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
